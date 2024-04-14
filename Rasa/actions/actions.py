f9c19b131c8eeb238569968eb0a7ff3ca20c189f
Aryan <aryan.saxena094@gmail.com>
2024-04-14 19:58:29 -0400

    actions.py

--- Rasa/actions/actions.py
+++ Rasa/actions/actions.py
@@ -3,34 +3,201 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-# Load SPARQL queries from a YAML file
+# Load SPARQL queries from the YAML file
 def load_queries():
-    with open('queries.yml') as file:
+    with open('queries.yml', 'r') as file:
         return yaml.safe_load(file)
 
 queries = load_queries()
 
-# General method to run SPARQL queries
+# Function to execute SPARQL queries
 def run_query(query, **kwargs):
     sparql = SPARQLWrapper("http://localhost:3030/dataset/query")
-    formatted_query = query.format(**kwargs)
-    sparql.setQuery(formatted_query)
+    sparql.setQuery(query.format(**kwargs))
     sparql.setReturnFormat(JSON)
-    return sparql.query().convert()
+    results = sparql.query().convert()
+    return results
 
-# Define an action for each intent that requires a SPARQL query
-class ActionQueryUniversityCourses(Action):
+# Example action using loaded queries
+class ActionListUniversityCourses(Action):
     def name(self):
-        return "action_query_university_courses"
+        return "action_list_university_courses"
 
     def run(self, dispatcher, tracker, domain):
         university = tracker.get_slot('university')
-        query = queries['queries']['list_university_courses']['query']
+        query = queries['list_university_courses']['query']
         results = run_query(query, university=university)
 
         courses = [result['course']['value'] for result in results['results']['bindings']]
         message = f"{university} offers the following courses: " + ", ".join(courses)
         dispatcher.utter_message(text=message)
         return []
+    
+class ActionFindCoursesByTopic(Action):
+    def name(self):
+        return "action_find_courses_by_topic"
 
-# Additional actions can be defined following the same pattern.
+    def run(self, dispatcher, tracker, domain):
+        topic = tracker.get_slot('topic')
+        query = queries['find_courses_by_topic']['query']
+        results = run_query(query, topic=topic)
+
+        courses = [result['course']['value'] for result in results['results']['bindings']]
+        message = f"The following courses cover the topic {topic}: " + ", ".join(courses)
+        dispatcher.utter_message(text=message)
+        return []
+
+class ActionListTopicsInCourse(Action):
+    def name(self):
+        return "action_list_topics_in_course"
+
+    def run(self, dispatcher, tracker, domain):
+        course = tracker.get_slot('course')
+        query = queries['list_topics_in_course']['query']
+        results = run_query(query, course=course)
+
+        topics = [result['topic']['value'] for result in results['results']['bindings']]
+        message = f"The course {course} covers the following topics: " + ", ".join(topics)
+        dispatcher.utter_message(text=message)
+        return []
+
+class ActionListCoursesBySubject(Action):
+    def name(self):
+        return "action_list_courses_by_subject"
+
+    def run(self, dispatcher, tracker, domain):
+        subject = tracker.get_slot('subject')
+        query = queries['list_courses_by_subject']['query']
+        results = run_query(query, subject=subject)
+
+        courses = [result['course']['value'] for result in results['results']['bindings']]
+        message = f"The following courses are related to the subject {subject}: " + ", ".join(courses)
+        dispatcher.utter_message(text=message)
+        return []
+
+class ActionRecommendedMaterialsForTopic(Action):
+    def name(self):
+        return "action_recommended_materials_for_topic"
+    
+    def run(self, dispatcher, tracker, domain):
+        topic = tracker.get_slot('topic')
+        query = queries['recommended_materials_for_topic']['query']
+        results = run_query(query, topic=topic)
+
+        materials = [result['material']['value'] for result in results['results']['bindings']]
+        message = f"Here are some recommended materials for the topic {topic}: " + ", ".join(materials)
+        dispatcher.utter_message(text=message)
+        return []
+
+class ActionCourseCredits(Action):
+    def name(self):
+        return "action_course_credits"
+    
+    def run(self, dispatcher, tracker, domain):
+        course = tracker.get_slot('course')
+        query = queries['course_credits']['query']
+        results = run_query(query, course=course)
+
+        credits = results['results']['bindings'][0]['credits']['value']
+        message = f"The course {course} is worth {credits} credits."
+        dispatcher.utter_message(text=message)
+        return []
+
+class ActionAdditionalCourseResources(Action):
+    def Name(self):
+        return "action_additional_course_resources"
+    
+    def run(self, dispatcher, tracker, domain):
+        course = tracker.get_slot('course')
+        query = queries['additional_course_resources']['query']
+        results = run_query(query, course=course)
+
+        resources = [result['resource']['value'] for result in results['results']['bindings']]
+        message = f"Here are some additional resources for the course {course}: " + ", ".join(resources)
+        dispatcher.utter_message(text=message)
+        return []
+
+class ActionDetailCourseContent(Action):
+    def name(self):
+        return "action_detail_course_content"
+    
+    def run(self, dispatcher, tracker, domain):
+        course = tracker.get_slot('course')
+        query = queries['detail_course_content']['query']
+        results = run_query(query, course=course)
+
+        content = results['results']['bindings'][0]['content']['value']
+        message = f"Here is the detailed content for the course {course}: {content}"
+        dispatcher.utter_message(text=message)
+        return []
+
+class ActionRecommendedReadingForTopic(Action):
+    def name(self):
+        return "action_recommended_reading_for_topic"
+    
+    def run(self, dispatcher, tracker, domain):
+        topic = tracker.get_slot('topic')
+        query = queries['recommended_reading_for_topic']['query']
+        results = run_query(query, topic=topic)
+
+        readings = [result['reading']['value'] for result in results['results']['bindings']]
+        message = f"Here is some recommended reading material for the topic {topic}: " + ", ".join(readings)
+        dispatcher.utter_message(text=message)
+        return []
+
+class ActionCompetenciesGained(Action):
+    def name(self):
+        return "action_competencies_gained"
+    
+    def run(self, dispatcher, tracker, domain):
+        course = tracker.get_slot('course')
+        query = queries['competencies_gained']['query']
+        results = run_query(query, course=course)
+
+        competencies = [result['competency']['value'] for result in results['results']['bindings']]
+        message = f"Upon completion of the course {course}, you will have gained the following competencies: " + ", ".join(competencies)
+        dispatcher.utter_message(text=message)
+        return []
+
+class ActionStudentGrades(Action):
+    def name(self):
+        return "action_student_grades"
+    
+    def run(self, dispatcher, tracker, domain):
+        student = tracker.get_slot('student')
+        query = queries['student_grades']['query']
+        results = run_query(query, student=student)
+
+        grades = [result['grade']['value'] for result in results['results']['bindings']]
+        message = f"Here are the grades for student {student}: " + ", ".join(grades)
+        dispatcher.utter_message(text=message)
+        return []
+
+class ActionStudentCompletedCourses(Action):
+    def name(self):
+        return "action_student_completed_courses"
+    
+    def run(self, dispatcher, tracker, domain):
+        student = tracker.get_slot('student')
+        query = queries['student_completed_courses']['query']
+        results = run_query(query, student=student)
+
+        courses = [result['course']['value'] for result in results['results']['bindings']]
+        message = f"Here are the courses completed by student {student}: " + ", ".join(courses)
+        dispatcher.utter_message(text=message)
+        return []
+
+class ActionPrintStudentTranscript(Action):
+    def name(self):
+        return "action_print_student_transcript"
+    
+    # We just return the course Number and the grade of the student
+    def run(self, dispatcher, tracker, domain):
+        student = tracker.get_slot('student')
+        query = queries['student_transcript']['query']
+        results = run_query(query, student=student)
+
+        transcript = [f"{result['course']['value']} - {result['grade']['value']}" for result in results['results']['bindings']]
+        message = f"Here is the transcript for student {student}: " + ", ".join(transcript)
+        dispatcher.utter_message(text=message)
+        return []
